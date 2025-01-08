e819941ca20b6ce9ae6f1dd12a8b9617d01ec12d
Dmitry <dmitronus2015@gmail.com>
2025-01-08 16:21:11 +0200

    add reviews slider adaptive

--- js/main.js
+++ js/main.js
@@ -4,5 +4,25 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 650,
+        settings: "unslick"
+      }
+    ]
   });
 });
